fda4dbe96c91de3b1b18e920bbc535847bc4aa2f
evsoncustodio <evsoncustodio@gmail.com>
2018-11-19 21:44:58 -0200

    Revert "Refactore Adocao in Pet"
    This reverts commit 5ff3e86d1d2300b010940ca1570be122e2e1bb61.

--- api/models/pet.js
+++ api/models/pet.js
@@ -13,6 +13,23 @@ module.exports = (api) => {
             type: 'ObjectId',
             ref: 'Abrigo'
         },
+        solicitacoes: [
+            {
+                type: 'ObjectId',
+                ref: 'Solicitacao'
+            }
+        ],
+        estado: {
+            type: 'String',
+            trim: true,
+            enum: [
+                'Não Disponível para Adoção',
+                'Disponível para Adoação',
+                'Avaliando os Candidatos à Adoção',
+                'Adotado'
+            ],
+            default: 'Não Disponível para Adoção'
+        },
         vacinacoes: [
             {
                 type: 'ObjectId',
@@ -83,32 +100,6 @@ module.exports = (api) => {
         castrado: {
             type: 'Boolean',
             required: true
-        },
-        adocao: {
-            solicitacoes: [
-                {
-                    type: 'ObjectId',
-                    ref: 'Solicitacao'
-                }
-            ],
-            estado: {
-                type: 'String',
-                trim: true,
-                enum: [
-                    'Não Disponível para Adoção',
-                    'Disponível para Adoação',
-                    'Avaliando os Candidatos à Adoção',
-                    'Adotado'
-                ],
-                default: 'Não Disponível para Adoção'
-            },
-            quantidadeCandidatos: {
-                type: 'Number',
-                default: 0
-            },
-            dataLimite: {
-                type: 'Date'
-            }
         },
         caracteristicas: {
             grauBrincalhao: {
