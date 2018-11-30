2fdf181bd85560d2951627319a90b6eb9afa422f
evsoncustodio <evsoncustodio@gmail.com>
2018-11-30 20:56:09 -0200

    Add QR Code Download

--- api/controllers/app.js
+++ api/controllers/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = (api) => {
     return {
         download: (req, res, next) => {
-            res.download(path.resolve(__dirname, './../../public/favicon.png'));
+            res.download(path.resolve(__dirname, './../../download/qrcode.png'));
         }
     }
 }
